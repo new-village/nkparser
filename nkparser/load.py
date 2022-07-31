77608554276971f7a9eb3219075fa8b45f17f2e5
jpnkni <kazuki.niimura@sas.com>
2022-08-01 02:08:54 +0900

    support odds api

--- nkparser/load.py
+++ nkparser/load.py
@@ -1,20 +1,26 @@
+'''load.py
+'''
 import logging
 from abc import ABCMeta, abstractmethod
 
 import requests
-from bs4 import BeautifulSoup
+
 
 # Set Logger
 logger = logging.getLogger('NkLoader')
 
 class NkLoader():
-    def __init__(self):   
+    ''' NkLoader
+    '''
+    def __init__(self):
         return
 
     def load(self, category, entity_id):
+        ''' load
+        '''
         loader = self._create_loader(category)
         return loader.load_html(entity_id)
-    
+
     def _create_loader(self, category):
         if category == "ENTRY":
             return EntryLoader()
@@ -24,67 +30,57 @@ class NkLoader():
             return HorseLoader()
         else:
             # Raise error and abort script
-            logger.error('Unexpected category type: %s' % (category))
+            logger.error('Unexpected category type: %s', category)
             raise SystemExit()
 
 
 class BaseLoader(metaclass=ABCMeta):
+    ''' BaseLoader
+    '''
     def load_html(self, entity_id):
+        ''' load_html
+        '''
+        return self._requests(self._create_url(entity_id))
+
+    def _requests(self, url):
         try:
-            url = self.base_url + entity_id
-            res = requests.get(url)
-            res.encoding = 'EUC_JP'
-            soup = BeautifulSoup(res.text, 'html.parser')
-        except requests.exceptions as e:
+            response = requests.get(url)
+            response.encoding = response.apparent_encoding
+        except requests.exceptions.RequestException as exp:
             # Exception error handling
             logger.error('Request is failure: Name, server or service not known')
-            logger.error('RequestsExceptions', e)
-            raise SystemExit()
+            raise SystemExit() from exp
 
         # Response Status Confirmation
-        if res.status_code not in [200]:
+        if response.status_code not in [200]:
             # HTTP Response is not 200 (Normal)
-            logger.error('Request to %s has been failure: %s' % (url, res.status_code))
+            logger.error('Request to %s has been failure: %s', url, response.status_code)
             raise SystemExit()
 
-        return self._confirm_data(soup)
+        return response.text
 
     @abstractmethod
-    def _confirm_data(self, soup):
+    def _create_url(self, entity_id):
         pass
 
 
 class EntryLoader(BaseLoader):
-    def __init__(self):
-        self.base_url = 'https://race.netkeiba.com/race/shutuba.html?race_id='
-
-    def _confirm_data(self, soup):
-        if len(soup.select('div.RaceName')) == 0:
-            soup = None
-            logger.info('Race page is not found by defined race ID.')
-            
-        return soup
-
+    ''' EntryLoader
+    '''
+    def _create_url(self, entity_id):
+        base_url = 'https://race.netkeiba.com/race/shutuba.html?race_id={ID}'
+        return base_url.replace('{ID}', entity_id)
 
 class OddsLoader(BaseLoader):
-    def __init__(self):
-        self.base_url = 'https://race.netkeiba.com/odds/index.html?type=b1&race_id='
-
-    def _confirm_data(self, soup):
-        if len(soup.select('div#odds_tan_block')) == 0:
-            soup = None
-            logger.info('Odds page is not found by defined race ID.')
-
-        return soup
-
+    ''' OddsLoader
+    '''
+    def _create_url(self, entity_id):
+        base_url = 'https://race.netkeiba.com/api/api_get_jra_odds.html?race_id={ID}&type=1'
+        return base_url.replace('{ID}', entity_id)
 
 class HorseLoader(BaseLoader):
-    def __init__(self):
-        self.base_url = 'https://db.netkeiba.com/horse/'
-
-    def _confirm_data(self, soup):
-        if len(soup.select('div.horse_title > h1')) == 0:
-            soup = None
-            logger.info('Horse page is not found by defined horse ID.')
-
-        return soup
+    ''' HorseLoader
+    '''
+    def _create_url(self, entity_id):
+        base_url = 'https://db.netkeiba.com/horse/{ID}'
+        return base_url.replace('{ID}', entity_id)
--- test/test_load.py
+++ test/test_load.py
@@ -1,49 +1,74 @@
+''' test_load.py
+'''
+import json
 import unittest
+
+from bs4 import BeautifulSoup
 from nkparser import load
 
+
 class TestNkLoader(unittest.TestCase):
+    ''' TestNkLoader
+    '''
     def setUp(self):
         self.loader = load.NkLoader()
 
+    def _load_title(self, race_id):
+        text = self.loader.load('ENTRY', race_id)
+        soup = BeautifulSoup(text, 'html.parser')
+        return soup.select_one('div.RaceName')
+
     def test_entry_normal(self):
-        # Load Hakodate Kinen page
-        soup = self.loader.load('ENTRY', "202202011211")
-        # Compare result
-        title = soup.select_one('div.RaceName').get_text().strip()
-        self.assertEqual(title, '函館記念')
+        ''' test_entry_normal
+        '''
+        # Load Arima Kinen page
+        soup = self._load_title('201206050810')
+        self.assertEqual(soup.get_text().strip(), '有馬記念')
 
     def test_entry_nonexistent_id(self):
+        ''' test_entry_nonexistent_id
+        '''
         # Load non-existent page
-        soup = self.loader.load('ENTRY', "202202011299")
-        # Compare result
+        soup = self._load_title('201206050899')
         self.assertIsNone(soup)
+
+    def _load_odds(self, race_id):
+        text = self.loader.load('ODDS', race_id)
+        return json.loads(text)['status']
 
     def test_odds_normal(self):
-        # Load Hayayako-odds page
-        soup = self.loader.load('ODDS', "202202011211")
-        # Compare result
-        horse_name = soup.select_one('td.Horse_Name').get_text().strip()
-        self.assertEqual(horse_name, 'ハヤヤッコ')
+        ''' test_odds_normal
+        '''
+        # Load Arima Kinen page
+        status = self._load_odds('201206050810')
+        self.assertEqual(status, 'result')
 
     def test_odds_nonexistent_id(self):
-        # Load Hayayako-odds page
-        soup = self.loader.load('ODDS', "202202011299")
-        # Compare result
-        self.assertIsNone(soup)
+        ''' test_odds_nonexistent_id
+        '''
+        # Load non-existent page
+        status = self._load_odds('201206050899')
+        self.assertEqual(status, 'middle')
+
+    def _load_horse(self, horse_id):
+        text = self.loader.load('HORSE', horse_id)
+        soup = BeautifulSoup(text, 'html.parser')
+        return soup.select_one('div.horse_title > h1')
 
     def test_horse_normal(self):
+        ''' test_horse_normal
+        '''
         # Load Deep Impact page
-        soup = self.loader.load('HORSE', '2002100816')
-        # Compare result
-        horse_name = soup.select_one('div.horse_title > h1').get_text().strip()
-        self.assertEqual(horse_name, 'ディープインパクト')
+        soup = self._load_horse('2002100816')
+        self.assertEqual(soup.get_text().strip(), 'ディープインパクト')
 
     def test_horse_nonexistent_id(self):
+        ''' test_horse_nonexistent_id
+        '''
         # Load non-exist horse page
-        soup = self.loader.load('HORSE', '2002100000')
-        # Compare result
+        soup = self._load_horse('2002100000')
         self.assertIsNone(soup)
 
 
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
