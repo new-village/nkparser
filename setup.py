9f21ba2ad91f4ccc1d56ee7d13d294bba44882fa
new-village <kazuki.niimura@gmail.com>
2022-08-06 13:55:30 +0000

    add MANIFEST

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name='nkparser',
     version='0.6',
+    author='new-village',
+    url='https://github.com/new-village/nkparser',
+    description='nkparser is a simple scraping library for netkeiba.com',
     install_requires=['requests', 'bs4', 'jq'],
 )
