7cebfea936a8a1886c9083a94f1a448c6ef53ba2
new-village <kazuki.niimura@gmail.com>
2022-08-13 09:35:31 +0000

    add long description

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@
 '''
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='nkparser',
     version='1.0',
     author='new-village',
     url='https://github.com/new-village/nkparser',
     description='nkparser is a simple scraping library for netkeiba.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['requests', 'bs4', 'jq'],
     packages=find_packages(),
     package_data={'': ['config/*.json']},
